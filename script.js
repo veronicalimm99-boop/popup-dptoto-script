2f6367b47008a202641e30dbc713c4994d5c03e8
veronicalimm99-boop <veronicalimm99@gmail.com>
2026-06-11 13:46:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+		window.addEventListener('DOMContentLoaded', () => {
+		  <!-- if (window.location.pathname === '/m/' || window.location.pathname === '/m/index.php') { -->
+			$('body').append(`
+			  <div id="pop-up-container" onclick="close_pop_up()">
+				<div id="bg-black" class="fade-in"></div>
+	  
+				<div id="pop-up-wrapper" class="swirl-in-fwd">
+				  <img
+					src="https://raintoto-hujan.b-cdn.net/pop%20raintoto%20gg.png"
+					alt="LINK RAINTOTO"
+				  />
+	  
+				  <div class="x-mark-container" onclick="close_pop_up()">X</div>
+				</div>
+			  </div>
+			`);
+	  
+			setTimeout(close_pop_up, 5000);
+	  
+			document.getElementById('pop-up-wrapper').addEventListener('click', e => {
+			  e.stopPropagation();
+			});
+		  <!-- } -->
+		});
+	  
+		const close_pop_up = () => {
+		  document.getElementById('pop-up-wrapper')?.classList.replace('swirl-in-fwd', 'swirl-out-bck');
+		  document.getElementById('bg-black')?.classList.replace('fade-in', 'fade-out');
+	  
+		  setTimeout(() => {
+			const pop_up_container = document.getElementById('pop-up-container');
+			pop_up_container?.parentElement.removeChild(pop_up_container);
+		  }, 1000);
+		};
